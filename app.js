36366b593a87d42880d854f9d08beefb0a32a268
Debashish-deb <ddeba32@yahoo.com>
2023-10-11 21:06:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//Create a variable named myAge//
+let myAge=37;
+//Create a variable named earlyYears and save the value 2 to it.//
+let earlyYears=2;
+earlyYears=(earlyYears*10);
+let laterYears=myAge-2;
+//Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.//
+laterYears=(laterYears*4);
+//Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears.//
+let myAgeInDogYears=earlyYears+laterYears;
+//Write your name as a string, call its built-in method .toLowerCase(), and store the result in a variable called myName.//
+let myName = 'Debashish Deb'.toLowerCase();
+console.log('My name is '+ myName+'.'+' I am '+myAge+' years old in human years which is '+myAgeInDogYears+' years old in dog years.')
+
+
+
+
+
+
+
+
